b582e83e497507e176ad340811bf5b7f25d9d56a
PartyICE <60338362+PartyICE@users.noreply.github.com>
2020-01-28 21:42:18 +0300

    first commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+
+class MainComponent extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoList: []
+        };
+        this.addNewTodo = this.addNewTodo.bind(this);
+    }
+
+    addNewTodo(newTodoTitle) {
+        const newTodo = {
+            title: newTodoTitle,
+            id: generateIdByTitle(newTodoTitle),
+            status: 'TODO'
+        };
+        this.setState({
+            todoList: this.state.map(item => item).unshift(newTodo)
+        });
+    }
+
+    render() {
+        return(
+            <Grid container
+                justify="center"
+            >
+                <Grid item
+                      xs={6}
+                >
+                    <Card>
+                        Test
+                    </Card>   
+                    </Grid>
+            </Grid>
+        );
+    }
+}
+
+export default MainComponent;
